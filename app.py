0ec7000238022ed21e7617018d871754cde5ebb0
Myxogastria0808 <r.rstudio.c@gmail.com>
2023-08-19 15:19:32 +0900

    Hello

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-import random
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 # グラフ可視化
 import plotly.graph_objects as go
+import random
 import pprint
-#~.iat[列,行]
+
+
 
 ######################初期集団の生成######################
 #[x_1, x_2, x_3]
 def GenOriginParty(min_rand, max_rand, number_of_element, number_of_party):
     party = [[random.randint(min_rand,max_rand) for _ in range(number_of_element)] for _ in range(number_of_party)]
-    print("The Origin Party")
-    pprint.pprint(party)
     return party
 
 
@@ -28,10 +30,6 @@ def GA(number_of_party, number_of_element, min_rand, max_rand, lower_limit, part
         for j in range(number_of_party):
             party_element.append(party[j][i])
         party_element_list.append(party_element)
-
-    print("Elemented ([[No.0 elements][No.1 elements]...])")
-    pprint.pprint(party_element_list)
-
     ####評価関数####
     for i in range(number_of_element):
         object = party_element_list[i]
@@ -73,11 +71,6 @@ def GA(number_of_party, number_of_element, min_rand, max_rand, lower_limit, part
     evaluation_value_list.append(result_3_list)
     evaluation_value_list.append(result_4_list)
     evaluation_value_list.append(result_5_list)
-
-    print("Evaluation Value ([[No.0 elements][No.1 elements]...])")
-    pprint.pprint(evaluation_value_list)
-
-
     #得点の集計
     total_point_list = []
     for i in range(number_of_party):
@@ -85,28 +78,15 @@ def GA(number_of_party, number_of_element, min_rand, max_rand, lower_limit, part
         for j in range(number_of_element):
             each_total_point = evaluation_value_list[j][i] + each_total_point
         total_point_list.append(each_total_point)
-
-    print("Total Score ([No.0 party score, No.1 party score, ...]])")
-    pprint.pprint(total_point_list)
-
     #パーティ番号の付与
     for i in range(number_of_party):
         party_point = total_point_list[i]
         total_point_list[i] = [party_point, i]
-
-    print("Add party ID ([[No.0 party ID, No.0 party score],[ No.1 party ID, No1 party score], ...]])")
-    pprint.pprint(total_point_list)
-
     ####選択の下準備####
     #ソートする
     sorted_data = sorted(total_point_list, reverse=True)
-
-    print("Sorted Data")
-    print(sorted_data)
-
     ####選択 (エリート戦略)####
     selected_data = sorted_data[:lower_limit]
-
     selected_party_list = []
     for i in range(lower_limit):
         for j in range(2):
@@ -115,20 +95,10 @@ def GA(number_of_party, number_of_element, min_rand, max_rand, lower_limit, part
             elif j==1:
                 selected_party = party[selected_data[i][j]]
         selected_party_list.append(selected_party)
-
-    print("Selected Party list")
-    pprint.pprint(selected_party_list)
-
     #ランダム生成
     child_list = [[random.randint(min_rand,max_rand) for _ in range(number_of_element)] for _ in range(number_of_party -lower_limit)]
-    print("赤ちゃん爆誕")
-    pprint.pprint(child_list)
-
     #合体(新しい母集団の完成)
     result = selected_party_list + child_list
-    print("合体（新しい母集団の完成）")
-    pprint.pprint(result)
-
     ####グラフ出力####
     #1位のグラフを出力する
     top_result = selected_party_list[0]
@@ -186,7 +156,6 @@ element_5 = []
 
 party = GenOriginParty(min_rand, max_rand, number_of_element, number_of_party)
 while gen <= generation:
-    print(f"第{gen}世代")
     party, top_result = GA(number_of_party, number_of_element, min_rand, max_rand, lower_limit, party)
     element_0.append(top_result[0])
     element_1.append(top_result[1])
@@ -274,5 +243,18 @@ fig.update_layout(
     ),
 )
 
+#Import css
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#Rendering
+app.layout = html.Div(children=[
+    html.H1(children='Hello World'),
 
-fig.show()
+    dcc.Graph(
+        id='multifunc-engine-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(port="2023", debug=True)
